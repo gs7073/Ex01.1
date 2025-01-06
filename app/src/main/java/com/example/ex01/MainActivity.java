4511dcec85fe3cd176bb734887c07d3c6e4b0f92
gs7073 <gs7073@bs.amalnet.k12.il>
2025-01-06 15:47:34 +0200

    first commit

--- app/src/main/java/com/example/ex01/MainActivity.java
+++ app/src/main/java/com/example/ex01/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.ex01;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,5 +17,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        Button btn;
+        btn = findViewById(R.id.btnClickMe);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btn.setText("Oh, yea, I’ve been clicked!\n");
+            }
+
+
+        });
         };
     }
